0b41899c5bddde224706ec6d67cccc54393f909a
Vince Bloise <vbloise3@gmail.com>
2019-02-11 21:30:36 -0500

    Changed deploy to acp.vincebloise.com

--- awsCertUi/buildDeploy.sh
+++ awsCertUi/buildDeploy.sh
@@ -14,5 +14,5 @@ cd ~/awsCert/awsCertUi/
 #build the site
 ng build --prod --aot
 #upload files
-aws s3 cp ./dist/awsCertUi s3://awscertification.com --recursive --acl public-read
+aws s3 cp ./dist/awsCertUi s3://acp.vincebloise.com --recursive --acl public-read
 #aws s3 cp ./dist/mbloiseResume3Ui s3://www.marinbloise.com --recursive --acl public-read
