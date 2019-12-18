9557e250314937698ec2b8a543a5c898d381c2d9
Vince Bloise <vbloise3@gmail.com>
2019-12-17 21:30:11 -0500

    added exam[ple python code file

---
+++ public/Exam Notes/sample_python_code.py
@@ -0,0 +1,16 @@
+# Rename observations and features for easier pandas method calls
+#housing_data.rename(columns = {0:"zero",
+#                               1:"one",
+#                               2:"two",
+#                               3:"three",
+#                               4:"four",
+#                               5:"five",
+#                               6:"six",
+#                               7:"seven"},
+#                               inplace = True)
+#housing_data.rename(index = {0: "zero",
+#                             1:"one",
+#                             2:"two",
+#                             3:"three",
+#                             4:"four"},
+#                             inplace = True)
